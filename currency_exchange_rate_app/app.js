d243e75102a5bdd1ccba774fa39865d225c7550a
Nasir Saeed <saeednasir483@gmail.com>
2024-08-06 17:41:31 +0500

    added project

---
+++ currency_exchange_rate_app/app.js
@@ -0,0 +1,91 @@
+let apiKeys = "your-api"
+let URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKeys}`
+
+let exchangeRateBtn = document.getElementById("exchange-rate")
+let amountInput = document.getElementById("amount")
+let dropdown = document.querySelectorAll(".dropdown select")
+let fromCurr = document.querySelector(".from select")
+let toCurr = document.querySelector(".to select")
+let result = document.querySelector("#result")
+
+
+const allCountryCode = async () => {
+    try {
+        for (let select of dropdown) {
+            let response = await fetch(URL)
+            let data = await response.json();
+
+            let allRates = data.rates;
+            for (codeList in allRates) {
+                let newOption = document.createElement("option")
+                newOption.innerText = codeList
+                newOption.value = codeList
+                select.append(newOption)
+                if (select.name === "from" && codeList === "USD") {
+                    newOption.selected = "selected"
+                } else if (select.name === "to" && codeList === "PKR") {
+                    newOption.selected = "selected"
+                }
+                // console.log(codeList)
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching currency data for dropdowns:', error);
+    }
+}
+
+
+exchangeRateBtn.addEventListener('click', (evt) => {
+    evt.preventDefault()
+
+    updateCurrency()
+})
+
+
+
+const updateCurrency = () => {
+    let valAmount = amountInput.value
+    console.log(valAmount)
+
+    if (valAmount === "" || valAmount < 1) {
+        valAmount = 1
+        amountInput.value = "1"
+    }
+    currencyExchange()
+
+}
+
+
+
+
+const currencyExchange = async () => {
+    let response = await fetch(URL)
+    let data = await response.json();
+
+    let allRates = data.rates;
+
+    let fromCurrency = fromCurr.value
+    let toCurrency = toCurr.value
+
+    let fromRate = allRates[fromCurrency]
+    let toRate = allRates[toCurrency]
+
+    let conversionRate = toRate / fromRate
+    let amount = parseFloat(amountInput.value)
+    let finalAmount = amount * conversionRate
+    result.innerText = `${amount} ${fromCurrency} = ${finalAmount.toFixed()} ${toCurrency}`
+    console.log(finalAmount)
+
+    // console.log(data.rates)
+
+    // for (const currencyKeys in allRates) {
+    //     if (allRates.hasOwnProperty(currencyKeys)) {
+    //         let currencyVal = allRates[currencyKeys];
+    //         console.log(`${currencyVal}`)
+    //         let final = amountInput.value * currencyVal
+    //         result.innerText = final
+    //     }
+    // }
+}
+
+allCountryCode()
